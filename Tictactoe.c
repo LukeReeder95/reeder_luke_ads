53259ac27ee835352a7ef770145c8bd33552691e
Luke <LukeReeder95@gmail.com>
2019-02-21 20:05:38 +0000

    Initial game working

--- Tictactoe.c
+++ Tictactoe.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+bool checkWin();
 void displayBoard();
 char square[9] = {'-', '-', '-', '-', '-', '-', '-', '-', '-'};
+
 int main()
 {
+    bool winner = false;
     int player = 1;
     displayBoard();
-    do
+    while (winner == false)
     {
-        printf("\nPlease enter the square you'd like to choose Player %d \n", player);
+        printf("\nPlease enter the square(between 1-9) you'd like to choose Player %d \n", player);
         int select;
         bool validInput = false;
         scanf("%d", &select);
@@ -17,7 +20,7 @@ int main()
         while(validInput == false)
         {
             // && square[select] != '-'
-            if (select >= 0 && select <= 8 && square[select] == '-')
+            if (select >= 1 && select <= 9 && square[select -1] == '-')
             {
                 validInput = true;
             }
@@ -32,20 +35,31 @@ int main()
         {
             if (player == 1)
             {
-                square[select] = 'x';
-                displayBoard();
+                square[select-1] = 'x';
                 player++;
             }
             else
             {
-                square[select] = 'o';
-                displayBoard();
+                square[select-1] = 'o';
                 player--;
+            }
+            displayBoard();
+            winner = checkWin();
+            if( winner == true)
+            {
+                if(player == 1)
+                {
+                    player++;
+                }
+                else
+                {
+                    player--;
+                }
             }
         }
     }
-    while(0 != 1);
-    
+
+    printf("\nCongratulations! Player %d wins the game!", player); 
     return 0;
 }
 
@@ -61,4 +75,25 @@ void displayBoard()
     printf("   |   |   \n");
     printf(" %c | %c | %c \n", square[6], square[7], square[8]);
     printf("   |   |   \n");
+}
+
+bool checkWin()
+{
+    if(square[0] == square[1] && square[1] == square[2] && square[0] != '-')
+        return true;
+    if (square[3] == square[4] && square[4] == square[5] && square[3] != '-')
+        return true;
+    if (square[6] == square[7] && square[7] == square[8] && square[6] != '-')
+        return true;
+    if (square[0] == square[3] && square[3] == square[6] && square[0] != '-')
+        return true;
+    if (square[1] == square[4] && square[4] == square[7] && square[1] != '-')
+        return true;
+    if (square[2] == square[5] && square[5] == square[8] && square[2] != '-')
+        return true;
+    if (square[0] == square[4] && square[4] == square[8] && square[0] != '-')
+        return true;
+    if (square[2] == square[4] && square[4] == square[6] && square[2] != '-')
+        return true;
+    return false;
 }
